7d0c748a2ae2c3a61740e080317b973ba51a50a7
Mark Kalinovits <mkalinovits@chemaxon.com>
2019-12-09 23:38:56 +0100

    Setup linting

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 export default function App() {
   return (
@@ -11,15 +11,10 @@ export default function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-};
+}
