c88c8b27265cee4300c2a8bca14c52ace52edc3c
Mark Kalinovits <mkalinovits@chemaxon.com>
2019-12-21 02:17:47 +0100

    Fixing footer style

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useNamedStyles } from "../core/useStyles";
+
+const styles = {
+  root: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "fit-content",
+    color: "#ddd",
+    "& .add": {
+      color: "red"
+    }
+  }
+};
 
 export default function Footer() {
+  const classes = useNamedStyles("Footer", styles);
   return (
-    <footer>
-      <span>dedicated to Freakuency FRPG</span>
-      <span>created by Moondancer</span>
+    <footer className={classes.root}>
+      <span>Dedicated to Freakuency FRPG</span> | <span>Created by Moondancer</span>
     </footer>
   );
 }
